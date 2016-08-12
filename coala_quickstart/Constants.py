26129dae715069a65da9f60e44d3b98c18abb4d4
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-08-12 15:27:29 +0200

    Update IMPORTANT_BEAR_LIST until CSS

--- coala_quickstart/Constants.py
+++ coala_quickstart/Constants.py
@@ -1,11 +1,11 @@
-IMPORTANT_BEAR_LIST = {"C": {"ClangBear",
-                             "ClangComplexityBear",
-                             "CSecurityBear",
-                             "GNUIndentBear"},
-                       "JavaScript": {"JSHintBear",
-                                      "JSComplexityBear"},
-                       "Java": {"JavaPMD",
-                                "CheckstyleBear"},
-                       "Python": {"PEP8Bear",
-                                  "PyLintBear",
-                                  "YapfBear"}}
+IMPORTANT_BEAR_LIST = {
+    'All': {'FilenameBear', 'InvalidLinkBear', 'LineCountBear', 'coalaBear'},
+    "C": {"GNUIndentBear", 'CSecurityBear', 'ClangComplexityBear'},
+    'C#': {'CPDBear', 'CSharpLintBear', 'SpaceConsistencyBear'},
+    'C++': {'GNUIndentBear', 'CPDBear', 'CPPCheckBear', 'CPPCleanBear',
+            'ClangComplexityBear'},
+    'CMake': {'CMakeLintBear', 'SpaceConsistencyBear'},
+    'CSS': {'CSSLintBear', 'SpaceConsistencyBear'},
+    "JavaScript": {"JSHintBear", "JSComplexityBear"},
+    "Java": {"JavaPMD", "CheckstyleBear"},
+    "Python": {"PEP8Bear"}}
