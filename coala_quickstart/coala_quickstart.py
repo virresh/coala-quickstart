aafd7bbcbb1b021d4c51d516b39761bd375f235a
John Vandenberg <jayvdb@gmail.com>
2016-11-16 09:21:02 +0700

    Add non-interactive mode
    A new command line option --non-interactive may be used to
    prevent prompts, using defaults, and generating a .coafile
    that is safe to use in non-interactive mode, as it disables
    all bears needing values for non-optional settings.
    
    The introduction of a command line parser requires that
    the parser is given to coalib.settings functions, otherwise
    it would instantiate the default coala command line parser
    which has many arguments that are either not appropropriate
    or not implemented in coala-quickstart.
    
    Closes https://github.com/coala/coala-quickstart/issues/39

--- coala_quickstart/coala_quickstart.py
+++ coala_quickstart/coala_quickstart.py
@@ -1,28 +1,63 @@
+import argparse
 import os
+
 from pyprint.ConsolePrinter import ConsolePrinter
 
 from coalib.output.printers.LogPrinter import LogPrinter
+
 from coala_utils.Question import ask_question
+
 from coala_quickstart.interaction.Logo import print_welcome_message
 from coala_quickstart.generation.Project import (
     valid_path, get_used_languages, print_used_languages)
 from coala_quickstart.generation.FileGlobs import get_project_files
 from coala_quickstart.generation.Bears import (
-    filter_relevant_bears, print_relevant_bears)
+    filter_relevant_bears,
+    print_relevant_bears,
+    get_non_optional_settings_bears,
+    remove_unusable_bears,
+)
 from coala_quickstart.generation.Settings import (
     generate_settings, write_coafile)
 
 
+def _get_arg_parser():
+    description = """
+coala-quickstart automatically creates a .coafile for use by coala.
+"""
+    arg_parser = argparse.ArgumentParser(
+        prog="coala-quickstart",
+        description=description,
+        add_help=True
+    )
+
+    arg_parser.add_argument(
+        '-C', '--non-interactive', const=True, action='store_const',
+        help='run coala-quickstart in non interactive mode')
+
+    arg_parser.add_argument(
+        '--ci', action='store_const', dest='non_interactive', const=True,
+        help='continuous integration run, alias for `--non-interactive`')
+
+    return arg_parser
+
+
 def main():
+    arg_parser = _get_arg_parser()
+    args = arg_parser.parse_args()
+
     printer = ConsolePrinter()
     log_printer = LogPrinter(printer)
 
     print_welcome_message(printer)
 
-    project_dir = ask_question(
-        "What is your project directory?",
-        default=os.getcwd(),
-        typecast=valid_path)
+    project_dir = os.getcwd()
+    if not args.non_interactive:
+        project_dir = ask_question(
+            "What is your project directory?",
+            default=project_dir,
+            typecast=valid_path)
+
     project_files, ignore_globs = get_project_files(
         log_printer,
         printer,
@@ -31,8 +66,13 @@ def main():
     used_languages = list(get_used_languages(project_files))
     print_used_languages(printer, used_languages)
 
-    relevant_bears = filter_relevant_bears(used_languages)
+    relevant_bears = filter_relevant_bears(used_languages, arg_parser)
     print_relevant_bears(printer, relevant_bears)
+
+    if args.non_interactive:
+        unusable_bears = get_non_optional_settings_bears(relevant_bears)
+        remove_unusable_bears(relevant_bears, unusable_bears)
+        print_relevant_bears(printer, relevant_bears, 'usable')
 
     settings = generate_settings(
         project_dir,
--- coala_quickstart/generation/Bears.py
+++ coala_quickstart/generation/Bears.py
@@ -7,7 +7,7 @@ from coalib.misc.DictUtilities import inverse_dicts
 from coalib.output.printers.LogPrinter import LogPrinter
 
 
-def filter_relevant_bears(used_languages):
+def filter_relevant_bears(used_languages, arg_parser=None):
     """
     From the bear dict, filter the bears per relevant language.
 
@@ -21,8 +21,12 @@ def filter_relevant_bears(used_languages):
     log_printer = LogPrinter(NullPrinter())
     used_languages.append(("All", 100))
 
-    all_bears_by_lang = {lang: set(inverse_dicts(*get_filtered_bears(
-        [lang], log_printer)).keys()) for lang, _ in used_languages}
+    all_bears_by_lang = {
+        lang: set(inverse_dicts(*get_filtered_bears([lang],
+                                                    log_printer,
+                                                    arg_parser)).keys())
+        for lang, _ in used_languages
+    }
 
     bears_by_lang = {}
     for lang in all_bears_by_lang:
@@ -40,7 +44,60 @@ def filter_relevant_bears(used_languages):
     return lang_bears
 
 
-def print_relevant_bears(printer, relevant_bears):
+def get_non_optional_settings(bears):
+    """
+    From the bear dict, get the non-optional settings.
+
+    :param bears:
+        A dict with language name as key and bear classes as value.
+    :return:
+        A dict with Bear class as key and bear non-optional settings as value.
+    """
+    non_optional_settings = {}
+    for language in bears:
+        for bear in bears[language]:
+            if bear not in non_optional_settings:
+                needed = bear.get_non_optional_settings()
+                # FIXME: This only finds non-optional settings for immediate
+                # dependencies.  See https://github.com/coala/coala/issues/3149
+                for bear_dep in bear.BEAR_DEPS:
+                    needed.update(bear_dep.get_non_optional_settings())
+
+                non_optional_settings[bear] = needed
+
+    return non_optional_settings
+
+
+def get_non_optional_settings_bears(bears):
+    """
+    Return tuple of bears with non optional settings.
+
+    :param unusable_bears:
+        A collection of Bear classes.
+    """
+    non_optional_settings = get_non_optional_settings(bears)
+    non_optional_settings = tuple(bear for bear, settings
+                                  in non_optional_settings.items()
+                                  if settings)
+    return non_optional_settings
+
+
+def remove_unusable_bears(bears, unusable_bears):
+    """
+    From the bears dict, filter the bears appearing in unusable_bears.
+
+    :param bears:
+        A dict with language name as key and bear classes as value.
+    :param unusable_bears:
+        A collection of Bear classes.
+    """
+    for language, language_bears in bears.items():
+        for bear in tuple(language_bears):
+            if bear in unusable_bears:
+                bears[language].remove(bear)
+
+
+def print_relevant_bears(printer, relevant_bears, label='relevant'):
     """
     Prints the relevant bears in sections separated by language.
 
@@ -49,9 +106,11 @@ def print_relevant_bears(printer, relevant_bears):
     :param relevant_bears:
         A dict with language name as key and bear classes as value.
     """
-    printer.print(BEAR_HELP)
+    if label == 'relevant':
+        printer.print(BEAR_HELP)
+
     printer.print("\nBased on the languages used in project the following "
-                  "bears have been identified to be relevant:")
+                  "bears have been identified to be %s:" % label)
     for language in relevant_bears:
         printer.print("    [" + language + "]", color="green")
         for bear in relevant_bears[language]:
